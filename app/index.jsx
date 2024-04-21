0ebc70209082f9a991ee64ccdab9445ae55c2570
tajwarsaiyeed <rjabid36@gmail.com>
2024-04-21 07:03:55 +0600

    chore: font setup

--- app/index.jsx
+++ app/index.jsx
@@ -4,7 +4,7 @@ import {Text, View} from 'react-native';
 const App = () => {
     return (
         <View className="flex-1 items-center justify-center bg-white">
-            <Text className={'text-3xl'}>Open up App.js to start working on your app!</Text>
+            <Text className={'font-pblack'}>Open up App.js to start working on your app!</Text>
             <StatusBar style="auto"/>
         </View>
     );
